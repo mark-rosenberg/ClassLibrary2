9dfba3a0e14a691c377aaa846933a9fce309afc3
Mark Rosenberg <mark.rosenberg@outlook.com>
2020-12-21 15:55:41 -0600

    Code

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        public string Count(int arg)
+        {
+            string results = "";
+            for (int i = 1; i <= arg; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    results += "Fizz Buzz";
+                    results += "\r\n";
+
+                }
+                else if (i % 3 == 0)
+                {
+                    results += "Fizz";
+                    results += "\r\n";
+                }
+                else if (i % 5 == 0)
+                {
+                    results += "Buzz";
+                    results += "\r\n";
+                }
+                else
+                {
+                    results += i.ToString();
+                    results += "\r\n";
+
+                }
+
+            }
+            return results;
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary2;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 cls1 = new Class1();
+            Console.WriteLine(cls1.Count(10000000));
+            Console.ReadKey();
+        }
+    }
+}
